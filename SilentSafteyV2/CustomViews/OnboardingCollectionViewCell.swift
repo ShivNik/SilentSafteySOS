e2552d8f32f9134669565a37964fd9bcb76541a9
ShivNik <63324743+ShivNik@users.noreply.github.com>
2022-07-06 10:47:42 -0500

    1. Added Intro View Controller 2. No storyboard being used 3. Basic Design Set-Up

---
+++ SilentSafteyV2/CustomViews/OnboardingCollectionViewCell.swift
@@ -0,0 +1,123 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  SilentSafteyV2
+//
+//  Created by Shivansh Nikhra on 7/6/22.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "OnboardingCollectionViewCell"
+    
+    private let myImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(31)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemRed
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(17)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemRed
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private var stackView: UIStackView!
+    
+    func setUpStackView() {
+        let stackViewElements = [titleLabel,descriptionLabel]
+        stackView = UIStackView(arrangedSubviews: stackViewElements)
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .black
+        
+        let imageView = UIImageView()
+        contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(imageLiteralResourceName: "greenPhone")
+        imageView.contentMode = .scaleAspectFit
+    
+        
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.font = titleLabel.font.withSize(31)
+        titleLabel.numberOfLines = 0
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .center
+        titleLabel.text = "Automated SOS Calls"
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        descriptionLabel.font = descriptionLabel.font.withSize(20)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textColor = .white
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.text = "Have fun with your friends and the fmaily this is really long on purpose"
+        
+        let stackViewElements = [titleLabel,descriptionLabel]
+        let stackView = UIStackView(arrangedSubviews: stackViewElements)
+        contentView.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .fill
+        stackView.spacing = 16
+        
+        
+        NSLayoutConstraint.activate([
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -40),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+        ])
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: stackView.topAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    func configure(onboardingObject: OnboardingObject) {
+        myImageView.image = onboardingObject.image
+        titleLabel.text = onboardingObject.title
+        descriptionLabel.text = onboardingObject.description
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
---
+++ SilentSafteyV2/Model/OnboardingObject.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingObject.swift
+//  SilentSafteyV2
+//
+//  Created by Shivansh Nikhra on 7/6/22.
+//
+
+import Foundation
+import UIKit
+
+struct OnboardingObject {
+    let image: UIImage
+    let title: String
+    let description: String
+}
