0b061e1ac782e4de2ac72456371f747b01d66f50
ShivNik <63324743+ShivNik@users.noreply.github.com>
2022-07-06 19:48:05 -0500

    1. Added Pre-test slide, validation, and refactored code

--- SilentSafteyV2/Controllers/Onboarding/PreTestViewController.swift
+++ SilentSafteyV2/Controllers/Onboarding/PreTestViewController.swift
@@ -9,90 +9,97 @@ import UIKit
 import SkyFloatingLabelTextField
 
 class PreTestViewController: UIViewController {
+    var validTextField = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         
-        let salGuide = self.view.safeAreaLayoutGuide
-        let button = UIButton()
-        button.backgroundColor = .systemRed
-        button.setTitle("Get Started!", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
+        let safeArea = self.view.safeAreaLayoutGuide
+        let button = ReusableUIElements.createButton(title: "Let's do it!")
         view.addSubview(button)
+        button.addTarget(self, action:#selector(continueToTestAction), for: .touchUpInside)
+        ReusableUIElements.buttonConstraints(button: button, safeArea: safeArea, bottomAnchorConstant: -80)
         
-        NSLayoutConstraint.activate([
-            button.widthAnchor.constraint(equalToConstant: 150),
-            button.heightAnchor.constraint(equalToConstant: 50),
-            
-            button.centerXAnchor.constraint(equalTo: salGuide.centerXAnchor, constant: 0),
-            button.bottomAnchor.constraint(equalTo: salGuide.bottomAnchor, constant: -80)
-            
-        ])
-        let directionsLabel = createLabel(title: "How to use the App", fontSize: 31)
+        let directionsLabel = ReusableUIElements.createLabel(fontSize: 31, text: "How to use the App")
         directionsLabel.font = UIFont.boldSystemFont(ofSize: 31)
         
-        let labels = [directionsLabel, createLabel(title: "1. Tap the SOS Button and start the call", fontSize: 20), createLabel(title: "2. Return back to the app once the phone call begins", fontSize: 20), createLabel(title: "3. Type any additional Messages into the Text-Field", fontSize: 20)]
+        let labels = [
+            directionsLabel,
+            ReusableUIElements.createLabel(fontSize: 20, text: "1. Tap the SOS Button and start the call"),
+            ReusableUIElements.createLabel(fontSize: 20, text: "2. Return back to the app once the phone call begins"),
+            ReusableUIElements.createLabel(fontSize: 20, text: "3. Type any additional Messages into the Text-Field")
+        ]
         
-        var stackView = UIStackView(arrangedSubviews: labels)
-        stackView.axis = .vertical
-     //   stackView.spacing = 20
-        stackView.distribution = .equalSpacing
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let stackView = ReusableUIElements.createStackView(stackViewElements: labels, spacing: 0, distributionType: .equalSpacing)
         self.view.addSubview(stackView)
     
         NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: salGuide.centerXAnchor, constant: 0),
+            stackView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor, constant: 0),
             stackView.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -50),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
         ])
         
-        let labelsTwo = [createLabel(title: "Step 3: Let's try it out", fontSize: 31), createLabel(title: "Enter a phone number you can test the app with", fontSize: 20)]
+    
+        let labelsTwo = [
+            ReusableUIElements.createLabel(fontSize: 31, text: "Step 3: Let's try it out"),
+            ReusableUIElements.createLabel(fontSize: 20, text: "Enter a phone number you can test the app with")
+        ]
 
-        let stackViewTwo = UIStackView(arrangedSubviews: labelsTwo)
-        stackViewTwo.axis = .vertical
-        stackViewTwo.distribution = .fillEqually
-        stackViewTwo.spacing = 20
-        stackViewTwo.translatesAutoresizingMaskIntoConstraints = false
+        let stackViewTwo = ReusableUIElements.createStackView(stackViewElements: labelsTwo, spacing: 20, distributionType: .fillEqually)
         view.addSubview(stackViewTwo)
         
         NSLayoutConstraint.activate([
-            stackViewTwo.centerXAnchor.constraint(equalTo: salGuide.centerXAnchor, constant: 0),
-            stackViewTwo.topAnchor.constraint(equalTo: salGuide.topAnchor, constant: 20),
+            stackViewTwo.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor, constant: 0),
+            stackViewTwo.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
             stackViewTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             stackViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
         
         
-        let phoneNumberSkyTextField = SkyFloatingLabelTextField()
-        phoneNumberSkyTextField.placeholder = "Enter a Phone Number"
-        phoneNumberSkyTextField.backgroundColor = .black
-        phoneNumberSkyTextField.textColor = .white
-        phoneNumberSkyTextField.translatesAutoresizingMaskIntoConstraints = false
-        phoneNumberSkyTextField.selectedTitleColor = .red
-        phoneNumberSkyTextField.selectedLineColor = .blue
+        let phoneNumberSkyTextField = ReusableUIElements.createSkyTextField(placeholder: "Enter phone number", title: "Enter Phone Number", id: "phoneNumber")
         view.addSubview(phoneNumberSkyTextField)
+        phoneNumberSkyTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         
         NSLayoutConstraint.activate([
-            phoneNumberSkyTextField.centerXAnchor.constraint(equalTo: salGuide.centerXAnchor, constant: 0),
+            phoneNumberSkyTextField.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor, constant: 0),
             phoneNumberSkyTextField.topAnchor.constraint(equalTo: stackViewTwo.bottomAnchor, constant: 40),
             phoneNumberSkyTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             phoneNumberSkyTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-        ])
+        ])  
     }
     
-    func createLabel(title: String, fontSize: Int) -> UILabel {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-         
-        label.text = title
-        label.font = label.font.withSize(CGFloat(fontSize))
-        label.textColor = .white
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        
-        return label
+    @objc
+    func continueToTestAction(button: UIButton) {
+        if(validTextField) {
+            print("Here")
+            // self.navigationController?.pushViewController(PreTestViewController(), animated: true)
+        } else {
+            button.setTitle("Try Again", for: .normal)
+        }
+    }
+    @objc func textFieldDidChange(_ textfield: UITextField) {
+        if let floatingLabelTextField = textfield as? SkyFloatingLabelTextField {
+            if(validatePhoneNumber(skyTextField: floatingLabelTextField)) {
+                floatingLabelTextField.errorMessage = ""
+                validTextField = true
+            }
+            else {
+                floatingLabelTextField.errorMessage = "Invalid Phone Number"
+                validTextField = false
+            }
+        }
+    }
+    
+    func validatePhoneNumber(skyTextField: SkyFloatingLabelTextField) -> Bool {
+        print(skyTextField.text!.count)
+        if skyTextField.text != "" && Double(skyTextField.text!) != nil {
+            if(skyTextField.text!.count == 10) {
+                return true
+            }
+        }
+        return false
     }
 }
